1a5db44f7cc156eed67913483d6660aa8e539f25
LGGray <81540431+LGGray@users.noreply.github.com>
2021-06-29 12:56:01 +1000

    Update sum_pvals.R

--- sum_pvals.R
+++ sum_pvals.R
@@ -9,9 +9,9 @@ df <- lapply(fnames, read.delim)
 # Read in all possible genes
 genes <- read.delim("~/datasets/OneK1k/M_vs_F_DEout/genes.txt", sep="\t", header = F)[-1,]
 
-# Creating fishers method function
-psumunif = function(x,n) 1/factorial(n) * sum(sapply(0:n, 
-           function(k) (-1)^k * choose(n,k) * ifelse(x > k,x-k,0)^(n)))
+# Methods of combining p-values:  
+fishersMethod = function(x) pchisq(-2 * sum(log(x)),df=2*length(x),lower=FALSE)
+psumunif = function(x,n) 1/factorial(n) * sum(sapply(0:n, function(k) (-1)^k * choose(n,k) * ifelse(x > k,x-k,0)^(n)))
 sumPvalsMethod = function(x) {
   n = length(x)
   if (n < 10) {
@@ -19,13 +19,16 @@ sumPvalsMethod = function(x) {
   } else {
     pnorm(sum(x),n/2,sqrt(n/12),lower=TRUE)
   }
-  }
+}
+binomMethod = function(x) pbinom(sum(x < .05),size=length(x),prob=.05,lower=FALSE)
 
 # Create outfile dataframe
-outfile <- as.data.frame(matrix(nrow=length(genes), ncol=2))
+outfile <- as.data.frame(matrix(nrow=length(genes), ncol=4))
 outfile[,1] <- genes
-colnames(outfile)[1] <- "gene"
-colnames(outfile)[2] <- "FDR"
+colnames(outfile)[1] <- "genes"
+colnames(outfile)[2] <- "Pval - sum"
+colnames(outfile)[3] <- "Pval - binom"
+colnames(outfile)[4] <- "Pval - fishers"
 
 # pull pvals, adjust then save to outfile
 pval <- c()
@@ -35,13 +38,17 @@ for (gene in genes){
     pval <- c(pval, df[[i]][grep(gene,df[[i]][,1]),2])
     if (i == length(df)){
       pval <- pval[!is.na(pval)]
-      pval <- p.adjust(pval, method="fdr")
+      # Need to do this across all the p-values at once, or you need to specify the number of p-values you are looking at  
+      # pval <- p.adjust(pval, method="fdr")
       outfile[grep(gene, outfile[,1]),2] <- sumPvalsMethod(pval)
+      outfile[grep(gene, outfile[,1]),3] <- binomMethod(pval)
+      outfile[grep(gene, outfile[,1]),4] <- fishersMethod(pval)
     }
   }
 }
 
-
-outfile <- subset(outfile, FDR <= 0.01)
+# Do the FDR calc here 
+padjusted = cbind(p.adjust(outfile[,2]), p.adjust(outfile[,3]), p.adjust(outfile[,4]))  
+outfile = cbind(outfile, padjusted)
 
 write.table(outfile, file="~/datasets/OneK1k/M_vs_F_DEout/DEGs.txt", sep="\t", row.names = F)
