e54f566d5da0c103c44397901b4f63494a1d852f
LGGray <81540431+LGGray@users.noreply.github.com>
2022-03-23 11:00:50 +1100

    Update with instructions

--- fgsea.R
+++ fgsea.R
@@ -1,50 +1,56 @@
+# Rscript to perform GSEA with fgsea
+# msigdbr is used to download the gene pathways from MSigDB
+# http://www.gsea-msigdb.org/gsea/msigdb/collections.jsp
+
 library(fgsea)
 library(msigdbr)
 library(ggplot2)
 library(data.table)
 
-#msigdbr_df <- msigdbr(species = "human", category = "C7", subcategory = "IMMUNESIGDB")
-msigdbr_df <- msigdbr(species = "human", category = "H")
+### MSigDB Hallmark gene sets
+# msigdbr_df <- msigdbr(species = "human", category = "H")
+### MSigDB immune gene sets
+# msigdbr_df <- msigdbr(species = "human", category = "C7", subcategory = "IMMUNESIGDB")
+
 pathways = split(x = msigdbr_df$gene_symbol, f = msigdbr_df$gs_name)
 
-setwd("~/datasets/OneK1k/C_vs_RA_DEout/nebula/")
-files <- list.files(pattern = "\\.RDS$")
+# Set to directory with edgeR output. Example filename 'B_memory.txt'
+setwd("")
+files <- list.files(pattern = "\\.txt$")
 for (i in 1:length(files)){
-  cell <- gsub(".RDS", "", files[i])
+  cell <- gsub(".txt", "", files[i])
   print(cell)
-  re <- readRDS(files[i])
-  results.ord <- re$summary[ order(re$summary[,"logFC_ccY"]), ]
-  ranks <- results.ord$p_ccY
-  names(ranks) <- results.ord$gene
-  ranks <- log2(ranks)
+  res <- read.delim(files[i], header=T)
+  ordered <- res[order(res$logFC),]
+  ranks <- ordered$logFC
+  names(ranks) <- ordered$gene
   
   fgseaRes <- fgsea(pathways, ranks, minSize=15, maxSize = 500)
-  fwrite(fgseaRes, file=paste0("../fgsea/",cell,".H.tsv"), sep="\t", sep2=c("", " ", ""))
+  fwrite(fgseaRes, file=paste0("output/path/",cell,".tsv"), sep="\t", sep2=c("", " ", ""))
 }
 
-
-files = list.files("../fgsea/", pattern = "*.H.tsv")
-plot_list = list()
-for (i in 1:length(files)){
-  cell <- gsub(".tsv", "", files[i])
-  print(cell)
-  fgseaRes <- read.delim(paste0("../fgsea/", files[i]))
-  sigpath <- subset(fgseaRes, pval <= 0.05)
-  sigpath.ord <- sigpath[order(sigpath[,"pval"]),]
-  sigpath$pathway <- gsub("^GSE\\d+_", "", sigpath$pathway)
-  
-  p <- ggplot(sigpath[1:20,], aes(x = pval, y = pathway)) + 
-    geom_point(aes(size = size, fill = pval), alpha = 0.75, shape = 21, stroke = 0.5) +
-    scale_fill_gradient(low = "white", high = "red") +
-    ggtitle(paste0(cell, "", " - Top 20 enriched H pathways")) +
-    xlab("Pvalue")
-  
-  plot_list[[i]] = p
-  
-}
-
-pdf("~/plots/fgsea/RA.H.cells.pdf", width = 14)
-for (i in 1:length(files)){
-  print(plot_list[[i]])
-}
-dev.off()
+# files = list.files("input/path/", pattern = ".tsv")
+# plot_list = list()
+# for (i in 1:length(files)){
+#   cell <- gsub(".tsv", "", files[i])
+#   print(cell)
+#   fgseaRes <- read.delim(paste0("datasets/OneK1k/C_vs_RA_DEout/fgsea/all/", files[i]))
+#   sigpath <- subset(fgseaRes, padj <= 0.05)
+#   sigpath.ord <- sigpath[order(sigpath[,"padj"]),]
+#   sigpath$pathway <- gsub("^GSE\\d+_", "", sigpath$pathway)
+#   
+#   p <- ggplot(sigpath[1:20,], aes(x = padj, y = pathway)) + 
+#     geom_point(aes(size = size, fill = padj), alpha = 0.75, shape = 21, stroke = 0.5) +
+#     scale_fill_gradient(low = "#e80200", high = "#284060") +
+#     ggtitle(paste0(cell, "", " - Top 20 enriched IMMUNESIGDB pathways")) +
+#     xlab("padj")
+#   
+#   plot_list[[i]] = p
+#   
+# }
+# 
+# pdf("~/plots/fgsea/all.C7.pdf", width = 14)
+# for (i in 1:length(files)){
+#   print(plot_list[[i]])
+# }
+# dev.off()
