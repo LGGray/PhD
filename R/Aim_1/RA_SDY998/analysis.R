2955c96ed3916421995eb5bcdb07696198feea58
LGray95 <lachlan.gray95@gmail.com>
2023-01-31 17:14:39 +1100

    add GSEA

--- R/Aim_1/RA_SDY998/analysis.R
+++ R/Aim_1/RA_SDY998/analysis.R
@@ -12,10 +12,10 @@ setwd('/directflow/SCCGGroupShare/projects/lacgra/autoimmune.datasets/RA_SDY998/
 # Load the data
 pbmc <- readRDS('pbmc.female.RDS')
 
-deg.list <- edgeR.list('psuedobulk', logfc=0.5)
-names(deg.list) <- gsub('.edgeR-LRT', '', names(deg.list))
+degs <- edgeR.list('psuedobulk', logfc=0)
+names(degs) <- gsub('.edgeR-LRT', '', names(degs))
 
-deg.metrics <- lapply(deg.list, function(x){
+deg.metrics <- lapply(degs, function(x){
     up.all <- x[which(x$logFC > 0),]
     down.all <- x[which(x$logFC < 0),]
     XCI <- subset(x, gene %in% rownames(chrX))
@@ -25,8 +25,33 @@ deg.metrics <- lapply(deg.list, function(x){
 }) %>% do.call(rbind, .) 
 deg.metrics
 
-# Celltype abundance
-levels(pbmc)
-table(pbmc$cellTypist, pbmc$condition)
+# Get DEGs for chrX
+lapply(degs, function(x){
+    subset(x, gene %in% rownames(chrX) & logFC > 0)
+})
+lapply(degs, function(x){
+    subset(x, gene %in% rownames(chrX) & logFC < 0)
+})
 
+# Count number of cells in each condition for each cell
+table(pbmc$condition, pbmc$cellTypist)
 
+ABC <- subset(pbmc, cellTypist == 'Age-associated B cells'))
+Bmem <- subset(pbmc, cellTypist == 'Memory B cells')
+Treg <- subset(pbmc, cellTypist == 'Regulatory T cells')
+Temra <- subset(pbmc, cellTypist == 'Tem/Temra cytotoxic T cells')
+Th <- subset(pbmc, cellTypist == 'Tem/Trm cytotoxic T cells' & condition == 'disease')
+
+cell.sum <- rowSums(Temra)
+sort(cell.sum[grep('^CD', names(cell.sum))])
+
+# Find cell type markers
+pbmc.disease <- subset(pbmc, condition == 'disease')
+cell.markers <- FindAllMarkers(pbmc.disease, assay='SCT', min.pct=0.25, logfc.threshold=0.25, only.pos=TRUE)
+
+markers.split <- split(cell.markers, cell.markers$cluster)
+lapply(markers.split, function(x){
+    x[1:10,7]
+})
+
+save(cell.markers, file='disease.cell.markers.RDS')
