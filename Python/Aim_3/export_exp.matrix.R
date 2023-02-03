5d874112b9804d53fefa542d8c654661906389ad
LGGray <l.gray@garvan.org.au>
2023-02-03 12:46:39 +1100

    export matrix with deg features

--- Python/Aim_3/export_exp.matrix.R
+++ Python/Aim_3/export_exp.matrix.R
@@ -22,7 +22,7 @@ print(table(pbmc$condition, pbmc$cellTypist))
 for(cell in levels(pbmc)){
     pbmc.subset <- subset(pbmc, cellTypist == cell)
     if (length(which(pbmc.subset$condition == 'control')) < 10 | length(which(pbmc.subset$condition == 'disease')) < 10) {
-        cat('Not enough samples. Skipping', cell)
+        cat('Not enough samples. Skipping', cell, '\n')
         next
     }
     class <- pbmc.subset$condition
@@ -37,15 +37,16 @@ print('Complete Matrix Exported')
 
 # Export the expression matrix subsetted by differentially expressed genes for each cell type. Check that there are at least 10 cells per condition
 for(cell in levels(pbmc)){
-    deg <- read.delim(paste0('psuedobulk/', gsub(' ', '_', sub('/',' ', cell)), '.edgeR-LRT.txt'))
-    genes <- subset(deg, FDR < 0.05 & abs(logFC) > 0.5)$gene
-    if(length(genes) > 0){
+    files = list.files('psuedobulk', pattern = 'edgeR-LRT.txt')
+    if(paste0(gsub('/|-| ', '_', cell), '.edgeR-LRT.txt') %in% files == FALSE){
         cat('No DEGs. Skipping', cell)
         next
     }
+    deg <- read.delim(paste0('psuedobulk/', gsub('/|-| ', '_', cell), '.edgeR-LRT.txt'))
+    genes <- subset(deg, FDR < 0.05 & abs(logFC) > 0.5)$gene
     pbmc.subset <- subset(pbmc, cellTypist == cell, features=genes)
     if (length(which(pbmc.subset$condition == 'control')) < 10 | length(which(pbmc.subset$condition == 'disease')) < 10) {
-        cat('Not enough samples. Skipping', cell)
+        cat('Not enough samples. Skipping', cell, '\n')
         next
     }
     class <- pbmc.subset$condition
