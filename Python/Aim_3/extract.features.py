b9e57a6795ad6ace45c8543fd4f3734aa0d30c80
LGGray <l.gray@garvan.org.au>
2023-02-15 10:26:46 +1100

    add extract.features.py and UC_GSE125527.snakemake

---
+++ Python/Aim_3/extract.features.py
@@ -0,0 +1,19 @@
+import pickle
+import pandas as pd
+import glob
+import os.path
+
+# If ML.models/featureas does not exist, create it
+if not os.path.exists('ML.models/features'):
+    os.makedirs('ML.models/features')
+
+# loop through the models in ML.models and save features as .txt file
+for file in glob.glob('ML.models/*.sav'):
+    print(file)
+    # load the model from disk
+    model = pickle.load(open(file, 'rb'))
+    # Save numpy.ndarray as txt file
+    df = pd.DataFrame(model.get_feature_names_out(), columns = ['Features'])
+    df.to_csv('ML.models/features/'+os.path.basename(file).replace('.sav', '.txt'), index = False, sep='\t')
+
+
