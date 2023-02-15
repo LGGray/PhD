81aa9b0b60c2276f203a214a6c43be9f503b788c
LGray95 <lachlan.gray95@gmail.com>
2023-02-16 10:55:45 +1100

    commit

--- functions/cpdb.files.R
+++ functions/cpdb.files.R
@@ -20,14 +20,14 @@ write.table(deg.df, 'cpdb/DEGs.tsv', sep='\t', row.names=F, quote = F)
 pbmc <- readRDS('pbmc.female.RDS')
 pbmc@meta.data$cellTypist <- gsub('/|-| ', '_', pbmc@meta.data$cellTypist)
 Idents(pbmc) <- pbmc@meta.data$cellTypist
-# cells <- unique(deg.df$cellTypist)
-# pbmc <- subset(pbmc, cellTypist %in% cells)
+cells <- unique(deg.df$cellTypist)
+pbmc <- subset(pbmc, cellTypist %in% cells)
 
-pbmc@meta.data$Cell = rownames(pbmc@meta.data)
-df = pbmc@meta.data[, c('Cell', 'cellTypist')]
+pbmc@meta.data$Cell <- rownames(pbmc@meta.data)
+df <- pbmc@meta.data[, c('Cell', 'cellTypist')]
 write.table(df, file ='cpdb/meta.tsv', sep = '\t', quote = F, row.names = F)
 
-SaveH5Seurat(pbmc, filename = "cpdb/pbmc.h5Seurat")
+SaveH5Seurat(pbmc, filename = "cpdb/pbmc.h5Seurat", overwrite = T)
 Convert("cpdb/pbmc.h5Seurat", dest = "h5ad", overwrite = T)
 
 # writeMM(pbmc@assays$SCT@data, file = 'cpdb/matrix.mtx')
--- functions/multifunctionality.R
+++ functions/multifunctionality.R
@@ -2,16 +2,13 @@ library(EGAD)
 
 fibroblast <- read.delim('psuedobulk/Fibroblasts.edgeR-LRT.txt')
 degs <- subset(fibroblast, abs(logFC) > 0.5 & FDR < 0.05)$gene
-gene.set <- clusterProfiler::read.gmt('../../gene.sets/c5.go.bp.v7.5.1.symbols.gmt')
-gene.set <- gene.set[,c(2,1)]
+gene.set <- clusterProfiler::read.gmt('../../gene.sets/c5.go.v2022.1.Hs.symbols.gmt')[,c(2,1)]
 
-gene2annot <- subset(gene.set, gene %in% degs)
-annotationlist <- unique(gene2annot$term)
-annotations <- make_annotations(gene.set, degs, gene.set$term)
+annotations <- make_annotations(gene.set, unique(gene.set$gene), unique(gene.set$term))
 mf <- calculate_multifunc(annotations)
 auc_mf <- auc_multifunc(annotations, mf$MF.rank)
+pdf('AUC_mf.pdf')
 hist <- plot_distribution(auc_mf, xlab="AUROC", med=FALSE, avg=FALSE)
+dev.off() 
 
-mf <- mf[order(mf$MF.rank, decreasing=T),]
-mf.chrX <- subset(mf, Gene %in% rownames(chrX))
-
+subset(mf, Gene %in% degs)
