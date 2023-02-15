1ad7409b243f23987b6be504ca4810a55a40d2bd
LGray95 <lachlan.gray95@gmail.com>
2023-02-16 09:18:25 +1100

    commit

--- functions/ML.metrics.df.R
+++ functions/ML.metrics.df.R
@@ -28,8 +28,8 @@ metrics$nchrX <- unlist(lapply(feature.list, function(x){
 write.table(metrics, 'exp.matrix/metrics/Metrics.combined.txt', row.names=FALSE, quote=F)
 
 # Read in confusion matrices and save as Rdata
-confusion.files <- list.files('exp.matrix/metrics', pattern=c('confusion'))
+confusion.files <- list.files('exp.matrix/metrics', pattern=c('confusion'), full.names=T)
 confusion.list <- lapply(confusion.files, read.csv, row.names=c('Control', 'Disease'), col.names=c('Control', 'Disease'))
-confusion.files <- gsub('_confusion|.csv', '', confusion.files)
+confusion.files <- gsub('_confusion|.csv', '', basename(confusion.files))
 names(confusion.list) <- confusion.files
 save(confusion.list, file='exp.matrix/metrics/confusion.matrix.Rdata')
