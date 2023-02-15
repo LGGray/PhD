b266457ff5673411bab6634b053a4341e1e82f46
LGGray <l.gray@garvan.org.au>
2023-02-15 16:25:13 +1100

    replace with EGAD function

--- functions/multifunctionality.R
+++ functions/multifunctionality.R
@@ -1,12 +1,17 @@
-# Function to calculate the multifunctionality of a gene
-# Based on the method described in Gillis et al 2011 doi:10.1371/journal.pone.0017258
+library(EGAD)
 
-multifunct <- function(gene.list, gene.set){
-    result <- lapply(gene.list, function(gene){
-        Num_in.i <- length(grep(gene, gene.set))
-        Num_out.i <- length(grep(gene, gene.set, invert = TRUE))
-        return 1/(Num_in.i * Num_out.i)
-    }
-}
+fibroblast <- read.delim('psuedobulk/Fibroblasts.edgeR-LRT.txt')
+degs <- subset(fibroblast, abs(logFC) > 0.5 & FDR < 0.05)$gene
+gene.set <- clusterProfiler::read.gmt('../../gene.sets/c5.go.bp.v7.5.1.symbols.gmt')
+gene.set <- gene.set[,c(2,1)]
 
+gene2annot <- subset(gene.set, gene %in% degs)
+annotationlist <- unique(gene2annot$term)
+annotations <- make_annotations(gene.set, degs, gene.set$term)
+mf <- calculate_multifunc(annotations)
+auc_mf <- auc_multifunc(annotations, mf$MF.rank)
+hist <- plot_distribution(auc_mf, xlab="AUROC", med=FALSE, avg=FALSE)
+
+mf <- mf[order(mf$MF.rank, decreasing=T),]
+mf.chrX <- subset(mf, Gene %in% rownames(chrX))
 
