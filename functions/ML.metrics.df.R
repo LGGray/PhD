e431df08b8c71413ffccc4e15511edfce7d22c7f
LGray95 <lachlan.gray95@gmail.com>
2023-02-16 09:11:27 +1100

    count chrX features

--- functions/ML.metrics.df.R
+++ functions/ML.metrics.df.R
@@ -1,10 +1,12 @@
 library(dplyr)
 
+load('../../datasets/XCI/chrX.Rdata')
+
 # Read in ML metrics and combine into a single data frame
-metric.files <- list.files('exp.matrix/metrics', pattern=c('metrics'))
+metric.files <- list.files('exp.matrix/metrics', pattern=c('metrics'), full.names=T)
 metrics.list <- lapply(metric.files, read.csv)
 
-metric.files <- gsub('_metrics|.csv', '', metric.files)
+metric.files <- gsub('_metrics|.csv', '', basename(metric.files))
 names(metrics.list) <- metric.files
 metrics <- bind_rows(metrics.list, .id='model')
 
@@ -16,11 +18,18 @@ feature.files <- feature.files[match(feature.files, metric.files)]
 
 metrics$nFeatures <- unlist(lapply(feature.list, nrow))
 
+# Count how many features are in chrX
+metrics$nchrX <- unlist(lapply(feature.list, function(x){
+    sum(x$Features %in% rownames(chrX))
+    }
+))
+
 # Write data frame file
-write.table(metrics, 'Metrics.combined.txt', row.names=FALSE, quote=F)
+write.table(metrics, 'exp.matrix/metrics/Metrics.combined.txt', row.names=FALSE, quote=F)
 
 # Read in confusion matrices and save as Rdata
 confusion.files <- list.files('exp.matrix/metrics', pattern=c('confusion'))
 confusion.list <- lapply(confusion.files, read.csv, row.names=c('Control', 'Disease'), col.names=c('Control', 'Disease'))
 confusion.files <- gsub('_confusion|.csv', '', confusion.files)
 names(confusion.list) <- confusion.files
+save(confusion.list, file='exp.matrix/metrics/confusion.matrix.Rdata')
