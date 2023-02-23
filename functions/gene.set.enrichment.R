9f550abe4850573541f74f81e7bdf613d4ca0029
LGGray <l.gray@garvan.org.au>
2023-02-23 14:50:57 +1100

    add gene.set.enrichment

---
+++ functions/gene.set.enrichment.R
@@ -0,0 +1,32 @@
+library(ggplot2)
+library(ggrepel)
+
+source('../../PhD/functions/edgeR.list.R')
+load('../../datasets/XCI/chrX.Rdata')
+
+edgeR <- edgeR.list('psuedobulk', filter=F)
+
+gene.set.enrichment <- function(list, gene.set, p.value){
+    lapply(list, function(x){
+        a <- length(intersect(x[x$FDR < p.value,1], gene.set))
+        b <- length(intersect(x[x$FDR > p.value,1], gene.set))
+        c <- length(setdiff(x[x$FDR < p.value,1], gene.set))
+        d <- length(setdiff(x[x$FDR > p.value,1], gene.set))
+        fisher.test(matrix(c(a,b,c,d), nrow=2), alternative='greater')
+    })
+}
+
+result <- gene.set.enrichment(edgeR, rownames(chrX), p.value=0.05)
+
+pvalue <- unlist(lapply(result, function(x) x$p.value))
+odds.ratio <- unlist(lapply(result, function(x) x$estimate))
+plot.data <- data.frame(celltype <- names(result), pvalue=pvalue, odds.ratio=odds.ratio)
+
+# dotplot x=odds.ratio, y=pvalue, color=pvalue, label=celltype
+ggplot(plot.data, aes(x=odds.ratio, y=-log10(pvalue), color=-log10(pvalue), label=celltype)) +
+    geom_point() +
+    geom_text_repel() +
+    scale_color_gradient(low='blue', high='red') +
+    theme_bw() +
+    theme(legend.position='none')
+dev.off()
