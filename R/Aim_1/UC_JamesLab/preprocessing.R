776dfdcd607536b4b733f8f3eea7cac310b9bedb
LGGray <l.gray@garvan.org.au>
2023-03-01 15:13:05 +1100

    c

--- R/Aim_1/UC_JamesLab/preprocessing.R
+++ R/Aim_1/UC_JamesLab/preprocessing.R
@@ -19,7 +19,9 @@ exp <- read.csv('exp_counts.csv', row.names = 1)
 exp <- data.frame(t(exp))
 
 # Create Seurat object
-pbmc <- CreateSeuratObject(counts = exp, meta.data = metadata)
+pbmc <- CreateSeuratObject(counts = exp)
+print(pbmc)
+pbmc@meta.data <- cbind(pbmc@meta.data, metadata)
 
 # Remove obvious bad quality cells
 pbmc <- initialQC(pbmc)
@@ -29,78 +31,78 @@ pdf('ddqc.plot.pdf')
 df.qc <- ddqc.metrics(pbmc)
 dev.off()
 
-# Filter out the low quality cells
-pbmc <- filterData(pbmc, df.qc)
+# # Filter out the low quality cells
+# pbmc <- filterData(pbmc, df.qc)
 
-# Split object by individual for doublet detection
-pbmc.split <- SplitObject(pbmc, split.by = "individual")
-# loop through samples to run DoubletFinder on each individual
-for (i in 1:length(pbmc.split)) {
-  # print the sample we are on
-  print(paste0("Sample ",i))
+# # Split object by individual for doublet detection
+# pbmc.split <- SplitObject(pbmc, split.by = "individual")
+# # loop through samples to run DoubletFinder on each individual
+# for (i in 1:length(pbmc.split)) {
+#   # print the sample we are on
+#   print(paste0("Sample ",i))
   
-  # Pre-process seurat object with standard seurat workflow
-  pbmc.sample <- NormalizeData(pbmc.split[[i]])
-  pbmc.sample <- FindVariableFeatures(pbmc.sample)
-  pbmc.sample <- ScaleData(pbmc.sample)
-  if(ncol(pbmc.sample) > 50){
-    pbmc.sample <- RunPCA(pbmc.sample)
-  } else{
-    pbmc.split[[i]] <- NULL
-    break
-  }
+#   # Pre-process seurat object with standard seurat workflow
+#   pbmc.sample <- NormalizeData(pbmc.split[[i]])
+#   pbmc.sample <- FindVariableFeatures(pbmc.sample)
+#   pbmc.sample <- ScaleData(pbmc.sample)
+#   if(ncol(pbmc.sample) > 50){
+#     pbmc.sample <- RunPCA(pbmc.sample)
+#   } else{
+#     pbmc.split[[i]] <- NULL
+#     break
+#   }
   
-  # Find significant PCs
-  stdv <- pbmc.sample[["pca"]]@stdev
-  sum.stdv <- sum(pbmc.sample[["pca"]]@stdev)
-  percent.stdv <- (stdv / sum.stdv) * 100
-  cumulative <- cumsum(percent.stdv)
-  co1 <- which(cumulative > 90 & percent.stdv < 5)[1]
-  co2 <- sort(which((percent.stdv[1:length(percent.stdv) - 1] - 
-                       percent.stdv[2:length(percent.stdv)]) > 0.1), 
-              decreasing = T)[1] + 1
-  min.pc <- min(co1, co2)
-  print(min.pc)
+#   # Find significant PCs
+#   stdv <- pbmc.sample[["pca"]]@stdev
+#   sum.stdv <- sum(pbmc.sample[["pca"]]@stdev)
+#   percent.stdv <- (stdv / sum.stdv) * 100
+#   cumulative <- cumsum(percent.stdv)
+#   co1 <- which(cumulative > 90 & percent.stdv < 5)[1]
+#   co2 <- sort(which((percent.stdv[1:length(percent.stdv) - 1] - 
+#                        percent.stdv[2:length(percent.stdv)]) > 0.1), 
+#               decreasing = T)[1] + 1
+#   min.pc <- min(co1, co2)
+#   print(min.pc)
   
-  # finish pre-processing
-  pbmc.sample <- RunUMAP(pbmc.sample, dims = 1:min.pc)
-  pbmc.sample <- FindNeighbors(object = pbmc.sample, dims = 1:min.pc)              
-  pbmc.sample <- FindClusters(object = pbmc.sample, resolution = 0.1)
+#   # finish pre-processing
+#   pbmc.sample <- RunUMAP(pbmc.sample, dims = 1:min.pc)
+#   pbmc.sample <- FindNeighbors(object = pbmc.sample, dims = 1:min.pc)              
+#   pbmc.sample <- FindClusters(object = pbmc.sample, resolution = 0.1)
   
-  # pK identification (no ground-truth)
-  sweep.list <- paramSweep_v3(pbmc.sample, PCs = 1:min.pc, num.cores = detectCores()/2)
-  sweep.stats <- summarizeSweep(sweep.list)
-  bcmvn <- find.pK(sweep.stats)
+#   # pK identification (no ground-truth)
+#   sweep.list <- paramSweep_v3(pbmc.sample, PCs = 1:min.pc, num.cores = detectCores()/2)
+#   sweep.stats <- summarizeSweep(sweep.list)
+#   bcmvn <- find.pK(sweep.stats)
   
-  # Optimal pK is the max of the bomodality coefficent (BCmvn) distribution
-  bcmvn.max <- bcmvn[which.max(bcmvn$BCmetric),]
-  optimal.pk <- bcmvn.max$pK
-  optimal.pk <- as.numeric(levels(optimal.pk))[optimal.pk]
+#   # Optimal pK is the max of the bomodality coefficent (BCmvn) distribution
+#   bcmvn.max <- bcmvn[which.max(bcmvn$BCmetric),]
+#   optimal.pk <- bcmvn.max$pK
+#   optimal.pk <- as.numeric(levels(optimal.pk))[optimal.pk]
   
-  ## Homotypic doublet proportion estimate
-  annotations <- pbmc.sample@meta.data$seurat_clusters
-  homotypic.prop <- modelHomotypic(annotations) 
-  nExp.poi <- round(optimal.pk * nrow(pbmc.sample@meta.data)) ## Assuming 7.5% doublet formation rate - tailor for your dataset
-  nExp.poi.adj <- round(nExp.poi * (1 - homotypic.prop))
+#   ## Homotypic doublet proportion estimate
+#   annotations <- pbmc.sample@meta.data$seurat_clusters
+#   homotypic.prop <- modelHomotypic(annotations) 
+#   nExp.poi <- round(optimal.pk * nrow(pbmc.sample@meta.data)) ## Assuming 7.5% doublet formation rate - tailor for your dataset
+#   nExp.poi.adj <- round(nExp.poi * (1 - homotypic.prop))
   
-  # run DoubletFinder
-  pbmc.sample <- doubletFinder_v3(seu = pbmc.sample, 
-                                   PCs = 1:min.pc, 
-                                   pK = optimal.pk,
-                                   nExp = nExp.poi.adj)
-  metadata <- pbmc.sample@meta.data
-  colnames(metadata)[ncol(metadata)] <- "doublet_finder"
-  pbmc.sample@meta.data <- metadata 
+#   # run DoubletFinder
+#   pbmc.sample <- doubletFinder_v3(seu = pbmc.sample, 
+#                                    PCs = 1:min.pc, 
+#                                    pK = optimal.pk,
+#                                    nExp = nExp.poi.adj)
+#   metadata <- pbmc.sample@meta.data
+#   colnames(metadata)[ncol(metadata)] <- "doublet_finder"
+#   pbmc.sample@meta.data <- metadata 
   
-  # subset and save
-  pbmc.singlets <- subset(pbmc.sample, doublet_finder == "Singlet")
-  pbmc.split[[i]] <- pbmc.singlets
-  remove(pbmc.singlets)
-}
+#   # subset and save
+#   pbmc.singlets <- subset(pbmc.sample, doublet_finder == "Singlet")
+#   pbmc.split[[i]] <- pbmc.singlets
+#   remove(pbmc.singlets)
+# }
 
-pbmc <- merge(x = pbmc.split[[1]],
-              y = c(pbmc.split[-1]),
-              project = "UC_GSE182270")
+# pbmc <- merge(x = pbmc.split[[1]],
+#               y = c(pbmc.split[-1]),
+#               project = "UC_GSE182270")
 
 # SCTransform
 pbmc <- SCTransform(pbmc, verbose = FALSE)
