5d95a77f0d7e8fda8898bd09a52e64dbd6fdb727
LGray95 <lachlan.gray95@gmail.com>
2023-03-06 08:04:44 +1100

    commit

--- functions/plotting.ML.R
+++ functions/plotting.ML.R
@@ -2,15 +2,16 @@ library(ggplot2)
 library(dplyr)
 library(UpSetR)
 
-load('../datasets/XCI/escapees.Rdata')
+load('../../datasets/XCI/escapees.Rdata')
+source('../../PhD/functions/edgeR-LRT.R')
 
 setwd('~/external/ClusterScratch/autoimmune.datasets/')
-metrics <- read.delim('UC_GSE182270/exp.matrix/metrics/Metrics.combined.txt')
+metrics <- read.delim('exp.matrix/metrics/Metrics.combined.txt')
 
 metrics <- metrics %>%
   mutate(clf=gsub('_.+', '', model),
          celltype=gsub('.+_|.chrX', '', model))
-pdf('UC_GSE182270/model.comparison.pdf', width = 15)
+pdf('model.comparison.pdf', width = 15)
 ggplot(metrics, aes(x=celltype, y=F1, fill=clf)) +
   geom_col(position = 'dodge') +
   theme(axis.text.x = element_text(angle=45, hjust = 1)) +
@@ -27,7 +28,7 @@ lapply(metrics.split, function(x) x[order(x$F1, decreasing = T),][1,]) %>%
   data.frame() -> best.model
 best.model
 
-files = paste0('UC_GSE182270/ML.models/features/', best.model$clf, '_model_', best.model$celltype, '.chrX.txt')
+files = paste0('ML.models/features/', best.model$clf, '_model_', best.model$celltype, '.chrX.txt')
 
 #files <- list.files('UC_GSE125527/ML.models/features/', full.names = T)
 feature.list <- lapply(files, function(x) read.delim(x)$Features)
@@ -40,6 +41,11 @@ df <- fromList(feature.list)
 rownames(df) <- unique(unlist(feature.list))
 df$sum <- rowSums(df)
 View(df)
-hits <- names(which(sort(table(unlist(feature.list))) > 16))
+hits <- names(which(sort(table(unlist(feature.list))) > 12))
 
 hits %in% rownames(escape)
+
+edgeR <- edgeR.list('psuedobulk', logfc=0.1)
+for(cell in names(feature.list)){
+  edgeR[[paste0(gsub('.+_model_', '', cell), 'edgeR-LRT')]]
+}
