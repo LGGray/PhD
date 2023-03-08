6de31a5862a596f1cc4681a213e300d61f5672e1
LGGray <l.gray@garvan.org.au>
2023-03-09 09:31:21 +1100

    commit

--- Python/Aim_1/magic.py
+++ Python/Aim_1/magic.py
@@ -14,4 +14,6 @@ data.head()
 approx_magic_op = magic.MAGIC(solver="approximate")
 data_magic = approx_magic_op.fit_transform(data, genes='all_genes')
 
-feather.write_dataframe(data_magic, "exp_magic.feather")
+data_magic.to_csv("exp_magic.csv")
+
+#feather.write_dataframe(data_magic, "exp_magic.feather")
