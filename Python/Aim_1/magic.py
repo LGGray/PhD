21484a057a811b53d347c409a8e7666964dde5e2
LGGray <l.gray@garvan.org.au>
2023-03-08 19:04:48 +1100

    commit

---
+++ Python/Aim_1/magic.py
@@ -0,0 +1,17 @@
+import magic
+import scprep
+
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import feather
+
+data = scprep.io.load_csv("raw.counts.csv", index_col=0)
+data = data.transpose()
+data.head()
+
+approx_magic_op = magic.MAGIC(solver="approximate")
+data_magic = approx_magic_op.fit_transform(data, genes='all_genes')
+
+feather.write_dataframe(data_magic, "exp_magic.feather")
