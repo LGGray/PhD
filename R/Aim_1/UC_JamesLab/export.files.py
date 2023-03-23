16632ccc477b756c04239105dfa7772de743131d
LGGray <l.gray@garvan.org.au>
2023-03-23 17:19:29 +1100

    commit

--- R/Aim_1/UC_JamesLab/export.files.py
+++ R/Aim_1/UC_JamesLab/export.files.py
@@ -2,8 +2,8 @@ import scanpy as sc
 import pandas as pd
 
 # Load the data
-adata = sc.read_h5ad('uc_vs_healthy_jameslab.h5ad')
-adata = adata.raw.to_adata()
+adata = sc.read_h5ad('uc_healthy_only_jameslab_og.h5ad')
+# adata = adata.raw.to_adata()
 
 metadata = pd.DataFrame(adata.obs)
 metadata.to_csv('metadata.csv')
