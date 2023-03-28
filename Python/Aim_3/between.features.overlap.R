c2f3da8c43831f9ddbc4ab690c44a32cfbab30d4
LGGray <l.gray@garvan.org.au>
2023-03-28 16:58:03 +1100

    commit

--- Python/Aim_3/between.features.overlap.R
+++ Python/Aim_3/between.features.overlap.R
@@ -12,6 +12,7 @@ UC1 <- readRDS('UC_GSE125527/pbmc.female.RDS')
 UC2 <- readRDS('UC_GSE182270/pbmc.female.RDS')
 
 celltype <- commandArgs(trailingOnly=TRUE)
+print(celltype)
 
 # Subset each seurat object to the cell type of interest
 AD <- subset(AD, cellTypist %in% celltype[[1]], features=rownames(chrX))
@@ -24,4 +25,4 @@ UC2 <- subset(UC2, cellTypist %in% celltype[[1]], features=rownames(chrX))
 # Find overlap between features in each seurat object
 feature.list <- list(AD=rownames(AD), MS=rownames(MS), pSS=rownames(pSS), SLE=rownames(SLE), UC1=rownames(UC1), UC2=rownames(UC2))
 common.features  <- names(which(table(unlist(feature.list)) == 6))
-write.table(common.features, paste0('../common.features/', gsub(' |-|/|' '.', celltype), '.chrX.txt'), sep='\t', quote=F, row.names=F, col.names=F)
+write.table(common.features, paste0('../common.features/', gsub(' |-|/|', '.', celltype), '.chrX.txt'), sep='\t', quote=F, row.names=F, col.names=F)
