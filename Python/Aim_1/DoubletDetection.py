b81ca5a1317b00c6f0af8a751de16ce786109fc4
LGGray <l.gray@garvan.org.au>
2023-04-18 13:32:35 +1000

    commit

--- Python/Aim_1/DoubletDetection.py
+++ Python/Aim_1/DoubletDetection.py
@@ -3,39 +3,35 @@ import doubletdetection
 import numpy as np
 import pandas as pd
 
+# Read in expression matrix
 adata = sc.read_10x_mtx('.')
+# Add metadata file
+metadata = pd.read_csv('cell_batch.tsv.gz', sep='\t', header=0)
+adata.obs['individual'] = metadata['batch'].values
+# Read in barcodes file
 barcodes = pd.read_csv('barcodes.tsv.gz', header=None)
 
 # remove "empty" genes
 sc.pp.filter_genes(adata, min_cells=1)
 
-clf = doubletdetection.BoostClassifier(
-    n_iters=50,
-    clustering_algorithm="louvain",
-    standard_scaling=True,
-    pseudocount=0.1,
-    n_jobs=-1,
-)
-doublets = clf.fit(adata.X).predict(p_thresh=1e-16, voter_thresh=0.5)
-doublet_score = clf.doublet_score()
+# run doubletdetection over each individual sample
+adata_list = []
+for individual in adata.obs['individual'].unique():
+    adata_sub = adata[adata.obs['individual'] == individual]
+    print('processing individual', individual)
+    clf = doubletdetection.BoostClassifier(n_iters=50, clustering_algorithm="louvain", standard_scaling=True, pseudocount=0.1, n_jobs=-1,)
+    
+    doublets = clf.fit(adata_sub.X).predict(p_thresh=1e-16, voter_thresh=0.5)
+    doublet_score = clf.doublet_score()
 
-adata.obs["doublet"] = doublets
-adata.obs["doublet_score"] = doublet_score
+    adata_sub.obs["doublet"] = doublets
+    adata_sub.obs["doublet_score"] = doublet_score
 
-# Convergence of coublet calls
-f = doubletdetection.plot.convergence(clf, save='convergence_test.pdf', show=True, p_thresh=1e-16, voter_thresh=0.5)
-#Doublets on UMAP
-sc.pp.normalize_total(adata)
-sc.pp.log1p(adata)
-sc.pp.highly_variable_genes(adata)
-sc.tl.pca(adata)
-sc.pp.neighbors(adata)
-sc.tl.umap(adata)
-# save image
-sc.pl.umap(adata, color=["doublet", "doublet_score"], save='doublet_detection.pdf')
-sc.pl.violin(adata, "doublet_score", save='doublet_score.pdf')
+    adata_list.append(adata_sub)
 
-results = pd.Series(doublets, name="DoubletDetection_DropletType")
+adata = adata_list[0].concatenate(adata_list[1:])
+
+results = pd.Series(adata.obs["doublets"], name="DoubletDetection_DropletType")
 dataframe = pd.concat([barcodes, results], axis=1)
 dataframe.columns = ["CellID", "DoubletDetection_DropletType"]
 dataframe.DoubletDetection_DropletType = dataframe.DoubletDetection_DropletType.replace(1.0, "doublet")
--- R/Aim_1/pSS_GSE157278/preprocessing.R
+++ R/Aim_1/pSS_GSE157278/preprocessing.R
@@ -1,27 +1,26 @@
 # Script for building Seurat object from GSE157278 data
-library(GEOquery)
 library(Seurat)
-library(SeuratDisk)
 library(magrittr)
 library(ddqcR)
-library(DoubletFinder)
-library(parallel)
-
-load('/directflow/SCCGGroupShare/projects/lacgra/datasets/XCI/chrY.Rdata')
-source('/directflow/SCCGGroupShare/projects/lacgra/R_code/functions/calc.min.pc.R')
 
 setwd('/directflow/SCCGGroupShare/projects/lacgra/autoimmune.datasets/pSS_GSE157278')
 
 # Read in features, barcodes and matrix in wd
 pbmc.data <- Read10X('.')
 # Creat Seurat object
-pbmc <- CreateSeuratObject(counts= pbmc.data)
+pbmc <- CreateSeuratObject(counts=pbmc.data)
 # Add Sample information to object
 metadata <- read.delim('cell_batch.tsv.gz')
 pbmc$individual <- metadata$batch
 pbmc$condition <- gsub('-[0-9]', '', metadata$batch)
 pbmc$condition <- ifelse(pbmc$condition == 'pSS', 'disease', 'control')
 pbmc$sex <- 'F'
+
+# Read in DoubletDetector results
+dd <- read.delim('DoubletDetection_doublets_singlets.tsv')
+dd <- dd[dd$DoubletDetection_DropletType == 'singlet',]
+# Remove doublets
+pbmc <- subset(pbmc, cells = dd[,1])
 
 # Remove obvious bad quality cells
 pbmc <- initialQC(pbmc)
@@ -34,75 +33,13 @@ dev.off()
 # Filter out the cells
 pbmc <- filterData(pbmc, df.qc)
 
-pbmc.split <- SplitObject(pbmc, split.by = "individual")
+# Impute data
 
-# loop through samples to run DoubletFinder on each individual
-for (i in 1:length(pbmc.split)) {
-  # print the sample we are on
-  print(paste0("Sample ",i))
-  
-  # Pre-process seurat object with standard seurat workflow
-  pbmc.sample <- NormalizeData(pbmc.split[[i]])
-  pbmc.sample <- FindVariableFeatures(pbmc.sample)
-  pbmc.sample <- ScaleData(pbmc.sample)
-  if(ncol(pbmc.sample) > 50){
-    pbmc.sample <- RunPCA(pbmc.sample)
-  } else{
-    pbmc.split[[i]] <- NULL
-    break
-  }
-  
-  # Find significant PCs
-  stdv <- pbmc.sample[["pca"]]@stdev
-  sum.stdv <- sum(pbmc.sample[["pca"]]@stdev)
-  percent.stdv <- (stdv / sum.stdv) * 100
-  cumulative <- cumsum(percent.stdv)
-  co1 <- which(cumulative > 90 & percent.stdv < 5)[1]
-  co2 <- sort(which((percent.stdv[1:length(percent.stdv) - 1] - 
-                       percent.stdv[2:length(percent.stdv)]) > 0.1), 
-              decreasing = T)[1] + 1
-  min.pc <- min(co1, co2)
-  print(min.pc)
-  
-  # finish pre-processing
-  pbmc.sample <- RunUMAP(pbmc.sample, dims = 1:min.pc)
-  pbmc.sample <- FindNeighbors(object = pbmc.sample, dims = 1:min.pc)              
-  pbmc.sample <- FindClusters(object = pbmc.sample, resolution = 0.1)
-  
-  # pK identification (no ground-truth)
-  sweep.list <- paramSweep_v3(pbmc.sample, PCs = 1:min.pc, num.cores = detectCores() - 1)
-  sweep.stats <- summarizeSweep(sweep.list)
-  bcmvn <- find.pK(sweep.stats)
-  
-  # Optimal pK is the max of the bomodality coefficent (BCmvn) distribution
-  bcmvn.max <- bcmvn[which.max(bcmvn$BCmetric),]
-  optimal.pk <- bcmvn.max$pK
-  optimal.pk <- as.numeric(levels(optimal.pk))[optimal.pk]
-  
-  ## Homotypic doublet proportion estimate
-  annotations <- pbmc.sample@meta.data$seurat_clusters
-  homotypic.prop <- modelHomotypic(annotations) 
-  nExp.poi <- round(optimal.pk * nrow(pbmc.sample@meta.data)) ## Assuming 7.5% doublet formation rate - tailor for your dataset
-  nExp.poi.adj <- round(nExp.poi * (1 - homotypic.prop))
-  
-  # run DoubletFinder
-  pbmc.sample <- doubletFinder_v3(seu = pbmc.sample, 
-                                  PCs = 1:min.pc, 
-                                  pK = optimal.pk,
-                                  nExp = nExp.poi.adj)
-  metadata <- pbmc.sample@meta.data
-  colnames(metadata)[ncol(metadata)] <- "doublet_finder"
-  pbmc.sample@meta.data <- metadata 
-  
-  # subset and save
-  pbmc.singlets <- subset(pbmc.sample, doublet_finder == "Singlet")
-  pbmc.split[[i]] <- pbmc.singlets
-  remove(pbmc.singlets)
-}
+# Normalise data
 
-pbmc <- merge(x = pbmc.split[[1]],
-              y = c(pbmc.split[-1]),
-              project = "pSS_GSE157278")
+
+
+
 
 # SCTransform
 pbmc <- SCTransform(pbmc, verbose = FALSE)
@@ -117,13 +54,6 @@ pbmc <- RunUMAP(pbmc, dims = 1:min.pc)
 pdf('seurat.clusters.DimPlot.pdf')
 DimPlot(pbmc, reduction='umap')
 dev.off()
-
-pbmc.markers <- FindAllMarkers(pbmc, only.pos=T, min.pct=0.25, logfc.threshold = 0.25)
-write.table(pbmc.markers, 'FindAllMarkers.txt', row.names=T, quote=F, sep='\t')
-
-# Export .h5ad file for cellTypist
-SaveH5Seurat(pbmc, filename = "pbmc.h5Seurat", overwrite = TRUE)
-Convert("pbmc.h5Seurat", dest = "h5ad", overwrite = TRUE)
 
 mtx <- as.matrix(GetAssayData(pbmc))
 write.csv(mtx, 'raw.counts.csv')
