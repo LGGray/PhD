fd0362a8ab6db4004e2d3308a0220567a6077534
LGGray <l.gray@garvan.org.au>
2023-07-12 15:20:12 +1000

    add condition

--- functions/export.loom.R
+++ functions/export.loom.R
@@ -2,12 +2,20 @@ library(Seurat)
 library(SeuratDisk)
 library(loomR)
 
-pbmc <- readRDS('pbmc.female.RDS')
+# if pbmc.loom exists then end the script
+if (file.exists("pbmc.loom")) {
+  print("pbmc.loom already exists")
+  q()
+} else {
+  pbmc <- readRDS('pbmc.female.RDS')
 
-expr <-GetAssayData(pbmc, slot = "counts")
+    expr <-GetAssayData(pbmc, slot = "counts")
 
-# Create a new loom file
-lfile <- create(filename = "pbmc.loom", data = expr)
-# Close the connection to the loom file
-lfile$disconnect()
+    # Create a new loom file
+    lfile <- create(filename = "pbmc.loom", data = expr)
+    # Close the connection to the loom file
+    lfile$disconnect()
+}
+
+
 
