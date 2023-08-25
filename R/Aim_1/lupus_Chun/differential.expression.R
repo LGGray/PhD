0cc9a69fe3657f28e9a0e58f4969ce3352e0abdc
LGGray <l.gray@garvan.org.au>
2023-08-25 12:01:15 +1000

    lupus DGE

--- R/Aim_1/lupus_Chun/differential.expression.R
+++ R/Aim_1/lupus_Chun/differential.expression.R
@@ -1,54 +1,58 @@
 library(edgeR)
 library(Seurat)
 library(qvalue)
-library(tidyverse)
+library(tibble)
 
-setwd("/directflow/SCCGGroupShare/projects/lacgra/autoimmune.datasets/lupus_Chun")
+if(dir.exists('differential.expression') != TRUE){dir.create('differential.expression')}
+if(dir.exists('differential.expression/edgeR') != TRUE){dir.create('differential.expression/edgeR')}
+if(dir.exists('differential.expression/MAST') != TRUE){dir.create('differential.expression/MAST')}
 
-ancestry = commandArgs(trailingOnly=TRUE)
+# Read in Seurat file
+pbmc <- readRDS('pbmc.female.RDS')
+pbmc$disease_state <- gsub('^na', 'control', pbmc$disease_state)
+pbmc$development_stage <- as.numeric(gsub('-.+', '', pbmc$development_stage))
 
-pbmc <- readRDS(paste0('pbmc.female.', ancestry, '.RDS'))
-DefaultAssay(pbmc) <- 'SCT' 
+cell <- levels(pbmc)[commandArgs(trailingOnly = TRUE)]
 
-for (cell in levels(pbmc)){
-  # Select cell type
-  print(cell)
-  # subset object by cell type
-  pbmc.cell <- subset(pbmc, cellTypist == cell)
+# Select cell type
+print(cell)
+# subset object by cell type
+pbmc.cell <- subset(pbmc, cellTypist == cell)
 
-  # check if there are enough cell in both conditions and skip if not
-  if(length(unique(pbmc.cell$condition)) != 2){
-    print("Not enough conditions")
-    next
-  } else {
-    table(pbmc.cell$condition, pbmc.cell$cellTypist)
-  }
+# Keep genes with expression in 5% of cells
+keep <- rowSums(pbmc.cell@assays$RNA@counts > 0) > ncol(pbmc.cell) * 0.05
+features <- names(keep[keep == T])
+pbmc.cell <- subset(pbmc.cell, features=features)
 
-  # Psuedobulk
-  individual <- as.factor(pbmc.cell$sample_uuid)
-  mm <- model.matrix(~ 0 + individual)
-  colnames(mm) <- levels(individual)
-  expr <- GetAssayData(object = pbmc.cell, slot = "counts") %*% mm
-
-  # edgeR-LRT
-  targets = unique(data.frame(group = pbmc.cell$condition,
-                      individual = pbmc.cell$sample_uuid))
-  targets <- targets[match(colnames(expr), targets$individual),]
-  design <- model.matrix(~group, data=targets)
-  y = DGEList(counts = expr, group = targets$group)
-  # Disease group as reference
-  y$samples$group <- factor(y$samples$group, levels = c('disease', 'control'))
-  y <- calcNormFactors(y, method='TMM')
-  y = estimateGLMRobustDisp(y, design,
-                            trend.method = 'auto')
-  fit <- glmQLFit(y, design)
-  lrt <- glmLRT(fit)
-  print(summary(decideTests(lrt)))
-  res = topTags(lrt, n = Inf) %>%
+# Psudobulking by summing counts
+expr <- AggregateExpression(pbmc.cell, group.by='individual', slot='counts')$RNA
+expr <- expr[,(colSums(expr) > 0)]
+# edgeR-QLFTest
+abundance <- as.data.frame.matrix(table(pbmc.subset$individual, pbmc.subset$cellTypist), row.names=NULL)
+colnames(abundance) <- 'celltype'
+groups <- cbind(groups, abundance)
+targets = unique(data.frame(individual = pbmc.cell$individual,
+                            group = pbmc.cell$condition,
+                            age = pbmc.cell$development_stage))
+targets <- cbind(targets, abundance)
+rownames(targets) <- NULL
+targets <- targets[match(colnames(expr), targets$individual),]
+design <- model.matrix(~ 0 + age + celltype + condition, data=targets)
+contrasts <- makeContrasts(disease_vs_control = conditiondisease - conditioncontrol,
+                           age_effect = development_stage,
+                           cell_type_effect = celltype,
+                           levels = design) 
+y = DGEList(counts = expr, group = targets$group)
+# Disease group as reference
+contrasts <- makeContrasts(disease_vs_control = groupdisease - groupcontrol, levels = design)
+y = estimateGLMRobustDisp(y, design, trend.method = 'auto')
+fit <- glmQLFit(y, design)
+qlf <- glmQLFTest(fit, contrast=contrasts)
+print(summary(decideTests(qlf)))
+res = topTags(qlf, n = Inf) %>%
     as.data.frame() %>%
     rownames_to_column('gene')
-  res$FDR <- qvalue(p = res$PValue)$qvalues
-  cell = gsub("/|-| ", "_", cell)
-  write.table(res, paste0("psuedobulk/",ancestry,'/' cell, ".edgeR-LRT.txt"),
-              row.names=F, sep="\t", quote = F)
-}
+    res$FDR <- qvalue(p = res$PValue)$qvalues
+cell = gsub("/|-| ", "_", cell)
+write.table(res, paste0("differential.expression/edgeR/", cell, ".txt"),
+            row.names=F, sep="\t", quote = F)
