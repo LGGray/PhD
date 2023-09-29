ddc51aef183942f6bbbd97217ccd6e3e4ad9c276
LGGray <l.gray@garvan.org.au>
2023-09-29 13:56:11 +1000

    commit

--- functions/EGAD.pseudobulk.R
+++ functions/EGAD.pseudobulk.R
@@ -2,6 +2,8 @@ library(EGAD)
 library(Seurat)
 library(dplyr)
 library(ggplot2)
+library(ComplexHeatmap)
+library(circlize)
 
 load('/directflow/SCCGGroupShare/projects/lacgra/CoExpNets/bin/run_GBA.Rdata')
 source('/directflow/SCCGGroupShare/projects/lacgra/CoExpNets/bin/helper_functions.r')
@@ -21,39 +23,54 @@ annotations <- make_annotations(hallmark, unique(hallmark$gene), unique(hallmark
 
 # Load data, subset for celltype and condition then psuedobulk
 pbmc <- readRDS(commandArgs(trailingOnly = TRUE)[1])
+assay <- commandArgs(trailingOnly = TRUE)[2]
 
 # Subset for cell type, remove lowly expressed genes, psuedobulk and perform EGAD
 result.list <- list()
 for(cell in levels(pbmc)){
+  print(cell)
   pbmc.subset <- subset(pbmc, cellTypist == cell)
+
+  if(length(unique(pbmc.cell$condition)) != 2){
+    print("Not enough conditions")
+    next
+  }
+
   # Keep genes with expression in 5% of cells
-  keep <- rowSums(pbmc.subset@assays$decontXcounts@data > 0) > ncol(pbmc.subset) * 0.05
+  keep <- rowSums(pbmc.subset@assays$assay@data > 0) > ncol(pbmc.subset) * 0.05
   features <- names(keep[keep == T])
   pbmc.subset <- subset(pbmc.subset, features=features)
 
   # Control data
   control <- subset(pbmc.subset, condition == 'control')
   # Psudobulking by summing counts
-  control.expr <- AggregateExpression(control, group.by='individual', slot='data')$decontXcounts
+  control.expr <- AggregateExpression(control, group.by='individual', slot='data')$assay
+  # Remove genes with stdev = 0
+  control.expr <- control.expr[apply(control.expr, 1, sd) > 0,]
 
   # Disease data
   disease <- subset(pbmc.subset, condition == 'disease')
   # Psudobulking by summing counts
-  disease.expr <- AggregateExpression(disease, group.by='individual', slot='data')$decontXcounts
+  disease.expr <- AggregateExpression(disease, group.by='individual', slot='data')$assay
+  # Remove genes with stdev = 0
+  disease.expr <- disease.expr[apply(disease.expr, 1, sd) > 0,]
 
   ### EGAD on Control ###
   # Calculate network edges using correlations and rank standardize
   control.network = EGAD::build_coexp_network(control.expr, rownames(control.expr))
+  saveRDS(control.network, paste0('EGAD/', gsub("/|-| ", "_", cell), '.control.network.RDS'))
   # Neighbor Voting
-  control.gba_auc_nv <- data.frame(neighbor_voting(annotations, control.network, nFold=3, output="AUROC"))
+  control.gba_auc_nv <- data.frame(neighbor_voting(annotations, control.network, nFold=10, output="AUROC"))
   # Node Degree
   control.nd <- data.frame(node_degree(control.network))
 
   ## EGAD on Disease ###
   # Calculate network edges using correlations and rank standardize
+
   disease.network = EGAD::build_coexp_network(disease.expr, gene.list=rownames(disease.expr))
+  saveRDS(disease.network, paste0('EGAD/', gsub("/|-| ", "_", cell), '.disease.network.RDS'))
   # Neighbor Voting
-  disease.gba_auc_nv <- data.frame(neighbor_voting(annotations, disease.network, nFold=3, output="AUROC"))
+  disease.gba_auc_nv <- data.frame(neighbor_voting(annotations, disease.network, nFold=10, output="AUROC"))
   # Node Degree
   disease.nd <- data.frame(node_degree(disease.network))
 
@@ -68,40 +85,57 @@ for(cell in levels(pbmc)){
   # Save results
   write.table(gba_auc_nv, paste0('EGAD/', gsub("/|-| ", "_", cell), '.gba.txt'), sep='\t', quote=F)
   write.table(nd.df, paste0('EGAD/', gsub("/|-| ", "_", cell), '.nd.txt'), sep='\t', quote=F)
-  # pdf(paste0('EGAD/',gsub("/|-| ", "_", cell), '.gba_auc.scatter.pdf'))
-  # ggplot(gba_auc_nv, aes(x=auc.control, y=auc.disease)) + 
-  #   geom_point(color=ifelse(gba_auc_nv$auc.disease > 0.8, "red", "black")) +
-  #   geom_text(aes(label=ifelse(auc.disease > 0.8, rownames(gba_auc_nv), "")), vjust=-1) +
-  #   labs(x="Control AUC", y="Disease AUC") + ggtitle(paste("Guilt by association AUC:", cell))
-  # dev.off()
 }
 
-# cell <- names(result.list)[15]
-# pdf(paste0('EGAD/',gsub("/|-| ", "_", cell), '.gba_auc.scatter.pdf'))
-#   ggplot(result.list[[cell]], aes(x=auc.control, y=auc.disease)) + 
-#     geom_point(color=ifelse(gba_auc_nv$auc.disease > 0.8, "red", "black")) +
-#     geom_text(aes(label=ifelse(auc.disease > 0.8, rownames(gba_auc_nv), "")), vjust=-1, hjust=1) +
-#     labs(x="Control AUC", y="Disease AUC") + ggtitle(paste("Guilt by association AUC:", cell))
-# dev.off()
+egad.files <- list.files('EGAD', pattern='gba.txt', full.names=T)
+egad.result <- lapply(egad.files, read.delim)
+names(egad.result) <- gsub('.gba.txt', '', basename(egad.files))
 
-# egad.files <- list.files('EGAD', pattern='txt', full.names=T)
-# egad.result <- lapply(egad.files, read.delim)
-# names(egad.result) <- gsub('.txt', '', basename(egad.files))
+for(cell in names(egad.result)){
+  pdf(paste0('EGAD/', cell, '.scatter.pdf'))
+  file <- egad.result[[cell]]
+  print(ggplot(file, aes(x=auc.control, y=auc.disease)) + 
+  geom_point(color=ifelse(file$auc.disease > 0.7 & file$auc.control < 0.6, "red", "black")) +
+  geom_text(aes(label=ifelse(auc.disease > 0.7 & auc.control < 0.6, rownames(file), "")), vjust = "inward", hjust = "inward") +
+  geom_hline(yintercept=0.7, linetype="dashed", color="blue") +
+  geom_vline(xintercept=0.6, linetype="dashed", color="blue") +
+  labs(x="Control AUC", y="Disease AUC") + ggtitle(paste("Guilt by association AUC:", cell)))
+  dev.off()
+}
 
-# disease.pathway <- dplyr::bind_rows(lapply(egad.result, function(x){
-#   tmp <- subset(x, auc.disease > 0.8 & auc.control < 0.8)
-#   tmp <- cbind(pathway=rownames(tmp), tmp)
-#   rownames(tmp) <- NULL
-#   return(tmp)
-#   }), .id='celltype')
-# disease.pathway
+disease.pathway <- dplyr::bind_rows(lapply(egad.result, function(x){
+  tmp <- subset(x, auc.disease > 0.7 & auc.control < 0.6)
+  tmp <- cbind(pathway=rownames(tmp), tmp)
+  rownames(tmp) <- NULL
+  return(tmp)
+  }), .id='celltype')
+disease.pathway
 
-# # identify terms in disease with high AUC
-# terms.flt <- rownames(subset(disease.gba_auc_nv, auc > 0.8))
-# if(length(terms.flt) > 0){
-#   lapply(terms.flt, function(x) {
-#     features <- subset(hallmark, term == x)$gene
-#     features <- features[features %in% rownames(chrX)]
-#     subset(multifunc_assessment, Gene %in% features)
-#   })
-# }
+chrX.pathway <- lapply(unique(disease.pathway$pathway), function(x){
+  features <- subset(hallmark, term == x)$gene
+  features[features %in% rownames(chrX)]
+})
+names(chrX.pathway) <- unique(disease.pathway$pathway)
+
+for(line in 1:nrow(disease.pathway)){
+  print(disease.pathway[line,])
+  features <- subset(hallmark, term == disease.pathway[line,'pathway'])$gene
+  chrX.features <- features[features %in% rownames(chrX)]
+  control.coexp <- readRDS(paste0('EGAD/',disease.pathway[line,'celltype'],'.control.network.RDS'))
+  control.coexp_gene.set <- control.coexp[colnames(control.coexp) %in% features, rownames(control.coexp) %in% features]
+  disease.coexp <- readRDS(paste0('EGAD/',disease.pathway[line,'celltype'],'.disease.network.RDS'))
+  disease.coexp_gene.set <- disease.coexp[colnames(disease.coexp) %in% features, rownames(disease.coexp) %in% features]
+  if(length(chrX.features) > 0){
+    print(chrX.features[chrX.features %in% rownames(control.coexp_gene.set)])
+  }
+  pdf(paste0('EGAD/',disease.pathway[line,'celltype'],'.', disease.pathway[line,'pathway'], '.control.heatmap.pdf'))
+  print(Heatmap(control.coexp_gene.set, name='Spearmans Rho', column_title = "Control", 
+  clustering_distance_rows = "spearman", clustering_distance_columns = 'euclidean', 
+  clustering_method_rows = "complete", clustering_method_columns = "complete", show_row_names = FALSE, show_column_names = FALSE))
+  dev.off()
+  pdf(paste0('EGAD/',disease.pathway[line,'celltype'],'.', disease.pathway[line,'pathway'], '.disease.heatmap.pdf'))
+  print(Heatmap(disease.coexp_gene.set, name='Spearmans Rho', column_title = "Disease", 
+  clustering_distance_rows = "spearman", clustering_distance_columns = 'euclidean',
+  clustering_method_rows = "complete", clustering_method_columns = "complete", show_row_names = FALSE, show_column_names = FALSE))
+  dev.off()
+}
