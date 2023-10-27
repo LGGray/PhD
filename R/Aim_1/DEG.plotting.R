b904201f93b66d5cd636c515ee5a8622c7b26646
LGGray <l.gray@garvan.org.au>
2023-10-27 17:05:15 +1100

    extra code

--- R/Aim_1/DEG.plotting.R
+++ R/Aim_1/DEG.plotting.R
@@ -93,3 +93,68 @@ keep <- lapply(result$geneID, function(x){
 result.chrX <- result[unlist(keep),]
 genes <- unlist(strsplit(result.chrX$geneID, '/'))
 genes[genes %in% rownames(chrX)]
+
+
+##############
+library(ComplexHeatmap)
+library(circlize)
+library(UpSetR)
+library(ggplot2)
+
+source('/directflow/SCCGGroupShare/projects/lacgra/PhD/functions/edgeR.list.R')
+source('/directflow/SCCGGroupShare/projects/lacgra/PhD/functions/replace.names.R')
+load('/directflow/SCCGGroupShare/projects/lacgra/datasets/XCI/chrX.Rdata')
+load('/directflow/SCCGGroupShare/projects/lacgra/datasets/XCI/escapees.Rdata')
+
+edgeR <- deg.list('differential.expression/edgeR', filter=F)
+deg <- deg.list('differential.expression/edgeR', logfc=0.5)
+
+deg.escape <- lapply(deg, function(x) x[x$gene %in% rownames(escape),])
+
+# Create matrix of logFC.disease_vs_control for each gene
+genes <- unique(unlist(lapply(deg.escape, function(x) x$gene)))
+mtx <- matrix(0, nrow=length(genes), ncol=length(deg.escape))
+rownames(mtx) <- genes
+colnames(mtx) <- replace.names(gsub('_', '.', names(deg.escape)))
+for (i in 1:length(deg.escape)) {
+  mtx[,i] <- deg.escape[[i]]$logFC.disease_vs_control[match(genes, deg.escape[[i]]$gene)]
+}
+mtx[is.na(mtx)] <- 0
+
+mtx.cor <- cor(mtx, method='spearman')
+mtx.cor[is.na(mtx.cor)] <- 0
+
+# Cor heatmap
+pdf('APR/escape.cor.pdf')
+Heatmap(mtx.cor, clustering_distance_rows = "euclidean", clustering_distance_columns = "euclidean",
+clustering_method_rows = "complete", clustering_method_columns = "complete", column_title = 'Primary Sjogren\'s Syndrome',
+col=colorRamp2(c(0, 0.5, 1), c("blue", "white", "red")), name="Spearman's rho", column_title_side = "top",
+row_names_gp = gpar(fontsize = 8), column_names_gp = gpar(fontsize = 8))
+dev.off()
+
+# PCA logFC matrix
+pca <- prcomp(t(mtx), center=T)
+pdf('APR/chrX.pca.pdf')
+ggplot(data.frame(pca$x), aes(x=PC1, y=PC2)) + geom_point() + theme_bw() +
+geom_text(aes(label=rownames(pca$x)), size=2, hjust=0, vjust=0)
+dev.off()
+
+library(biomaRt)
+
+rownames(mtx) <- gsub('FAM122B', 'PABIR2', rownames(mtx))
+rownames(mtx) <- gsub('CXorf40A', 'EOLA1', rownames(mtx))
+rownames(mtx) <- gsub('CXorf40B', 'EOLA2', rownames(mtx))
+rownames(mtx) <- gsub('TAZ', 'TAFAZZIN', rownames(mtx))
+
+mart <- useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl")
+gene_info <- getBM(attributes = c("start_position", "external_gene_name", "chromosome_name"), filters = "external_gene_name", values = rownames(mtx), mart = mart)
+gene_info <- subset(gene_info, chromosome_name == 'X')
+gene_info <- gene_info[order(gene_info$start_position, decreasing=FALSE),]
+
+mtx.ordered <- mtx[match(gene_info$external_gene_name, rownames(mtx)),]
+
+pdf('APR/chrX.heatmap.pdf')
+Heatmap(mtx.ordered, cluster_rows = FALSE, name="logFC", column_title = '',
+col=colorRamp2(c(-0.5, 0, 0.5), c("blue", "white", "red")), rect_gp = gpar(col = "white", lwd = 2),
+show_row_names = FALSE)
+dev.off()
