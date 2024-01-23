820d02c54d3c5aafb14c49582bea1c8a6121f9b9
LGGray <l.gray@garvan.org.au>
2024-01-23 21:11:18 +1100

    Add libraries and modify directory structure for edgeR cell count analysis

--- functions/differential.expression.R
+++ functions/differential.expression.R
@@ -3,10 +3,16 @@ library(Seurat)
 library(MAST)
 library(qvalue)
 library(tidyverse)
+library(ggplot2)
+library(gplots)
+library(ComplexHeatmap)
+library(circlize)
+library(UpSetR)
+library(reshape2)
+library(clusterProfiler)
 
 if(dir.exists('differential.expression') != TRUE){dir.create('differential.expression')}
-if(dir.exists('differential.expression/edgeR') != TRUE){dir.create('differential.expression/edgeR')}
-if(dir.exists('differential.expression/MAST') != TRUE){dir.create('differential.expression/MAST')}
+if(dir.exists('differential.expression/edgeR_cellCount') != TRUE){dir.create('differential.expression/edgeR_cellCount')}
 
 # Read in file from command line
 pbmc <- readRDS(commandArgs(trailingOnly = TRUE)[1])
@@ -24,24 +30,25 @@ for (cell in levels(pbmc)){
 
   targets = unique(data.frame(group = pbmc.cell$condition,
                       individual = pbmc.cell$individual))
+  targets$cellCount <- sapply(targets$individual, function(id) sum(pbmc.cell$individual == id))
   targets <- targets[match(colnames(expr), targets$individual),]
   rownames(targets) <- targets$individual
-  design <- model.matrix(~0 + group, data=targets)
+  design <- model.matrix(~0 + cellCount + group, data=targets)
   y = DGEList(counts = expr, group = targets$group)
   # Disease group as reference
-  contrasts <- makeContrasts(disease_vs_control = groupdisease - groupcontrol, levels = design)
+  contrasts <- makeContrasts(disease_vs_control = groupdisease - groupcontrol,
+                            cell_type_effect = cellCount, levels = design)
   y <- calcNormFactors(y, method='TMM')
   y = estimateGLMRobustDisp(y, design, trend.method = 'auto')
   fit <- glmQLFit(y, design)
-  contrast_matrix <- contrasts[ ,c("disease_vs_control")]
-  qlf <- glmQLFTest(fit, contrast=contrast_matrix)
+  qlf <- glmQLFTest(fit, contrast=contrasts)
   print(summary(decideTests(qlf)))
   res = topTags(qlf, n = Inf) %>%
     as.data.frame() %>%
     rownames_to_column('gene')
   res$FDR <- qvalue(p = res$PValue)$qvalues
   cell = gsub("/|-| ", "_", cell)
-  write.table(res, paste0("differential.expression/edgeR/", cell, ".txt"),
+  write.table(res, paste0("differential.expression/edgeR_cellCount/", cell, ".txt"),
               row.names=F, sep="\t", quote = F)
 }
 
