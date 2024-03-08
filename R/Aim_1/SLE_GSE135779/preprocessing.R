73d5a5d265e5d8c5848ff5222bda2fdcc8ee227b
LGGray <l.gray@garvan.org.au>
2024-03-08 14:19:04 +1100

    Refactor preprocessing and clustering code

--- R/Aim_1/SLE_GSE135779/preprocessing.R
+++ R/Aim_1/SLE_GSE135779/preprocessing.R
@@ -42,78 +42,76 @@ setwd('/directflow/SCCGGroupShare/projects/lacgra/autoimmune.datasets/SLE_GSE135
 #   pbmc <- merge(pbmc, y = seurat_objects[[i]])
 # }
 
-# saveRDS(pbmc, 'pbmc.RDS')
-
 pbmc <- readRDS('pbmc.RDS')
 
-# Remove obvious bad quality cells
-pbmc <- initialQC(pbmc)
-# Return dataframe of filtering statistics
-pdf('ddqc.plot.pdf')
-df.qc <- ddqc.metrics(pbmc)
+# # Remove obvious bad quality cells
+# pbmc <- initialQC(pbmc)
+# # Return dataframe of filtering statistics
+# pdf('ddqc.plot.pdf')
+# df.qc <- ddqc.metrics(pbmc)
+# dev.off()
+# # Filter out the cells
+# pbmc <- filterData(pbmc, df.qc)
+
+# # remove doublets
+# sce <- as.SingleCellExperiment(pbmc)
+# sce <- scDblFinder(sce, samples="individual", clusters=TRUE, BPPARAM=MulticoreParam(3))
+# pbmc$scDblFinder <- sce$scDblFinder.class
+# pbmc <- subset(pbmc, scDblFinder == 'singlet')
+
+# # Normalise data with Delta method-based variance stabilizing
+# exp.matrix <- GetAssayData(pbmc, slot = 'counts', assay='RNA')
+# exp.matrix.transformed <- acosh_transform(exp.matrix)
+
+# # Add transformed data to Seurat object
+# pbmc <- SetAssayData(object=pbmc, assay='RNA', slot = 'data', new.data=exp.matrix.transformed)
+
+# # Cell type clustering and inspection of markers
+# pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+# pbmc <- ScaleData(pbmc)
+# pbmc <- RunPCA(pbmc)
+
+# # Find the number of PCs to use
+# # Plot the elbow plot
+# pdf('seurat.pca.pdf')
+# ElbowPlot(pbmc, ndims = 50)
+# dev.off()
+
+# # Plot PCA to show individual 
+# pdf('seurat.pca.individual.pdf')
+# DimPlot(pbmc, reduction='pca', group.by='individual')
+# dev.off()
+# # Or condition effects
+# pdf('seurat.pca.condition.pdf')
+# DimPlot(pbmc, reduction='pca', group.by='condition')
+# dev.off()
+
+# # Determine percent of variation associated with each PC
+# pct <- pbmc[["pca"]]@stdev / sum(pbmc[["pca"]]@stdev) * 100
+# # Calculate cumulative percents for each PC
+# cumu <- cumsum(pct)
+# # Determine which PC exhibits cumulative percent greater than 90% and % variation associated with the PC as less than 5
+# co1 <- which(cumu > 90 & pct < 5)[1]
+# # Determine the difference between variation of PC and subsequent PC
+# co2 <- sort(which((pct[1:length(pct) - 1] - pct[2:length(pct)]) > 0.1), decreasing = T)[1] + 1
+# # Minimum of the two calculation
+# pcs <- min(co1, co2)
+# print(paste('Selected # PCs', pcs))
+
+
+pbmc <- FindNeighbors(pbmc, dims=1:16)
+
+library("leiden")
+leiden_clustering <- leiden(pbmc@graphs$RNA_snn, resolution_parameter = 0.3)
+pbmc@meta.data$leiden_clustering <- leiden_clustering
+Idents(pbmc) <- 'leiden_clustering'
+pbmc <- RunUMAP(pbmc, dims = 1:16)
+
+pdf('seurat.clusters.DimPlot.pdf')
+DimPlot(pbmc, reduction='umap', label=TRUE)
 dev.off()
-# Filter out the cells
-pbmc <- filterData(pbmc, df.qc)
-
-# remove doublets
-sce <- as.SingleCellExperiment(pbmc)
-sce <- scDblFinder(sce, samples="individual", clusters=TRUE, BPPARAM=MulticoreParam(3))
-pbmc$scDblFinder <- sce$scDblFinder.class
-pbmc <- subset(pbmc, scDblFinder == 'singlet')
-
-# Normalise data with Delta method-based variance stabilizing
-exp.matrix <- GetAssayData(pbmc, slot = 'counts', assay='RNA')
-exp.matrix.transformed <- acosh_transform(exp.matrix)
-
-# Add transformed data to Seurat object
-pbmc <- SetAssayData(object=pbmc, assay='RNA', slot = 'data', new.data=exp.matrix.transformed)
-
-# Cell type clustering and inspection of markers
-pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
-pbmc <- ScaleData(pbmc)
-pbmc <- RunPCA(pbmc)
-
-# Find the number of PCs to use
-# Plot the elbow plot
-pdf('seurat.pca.pdf')
-ElbowPlot(pbmc, ndims = 50)
-dev.off()
-
-# Plot PCA to show individual 
-pdf('seurat.pca.individual.pdf')
-DimPlot(pbmc, reduction='pca', group.by='individual')
-dev.off()
-# Or condition effects
-pdf('seurat.pca.condition.pdf')
-DimPlot(pbmc, reduction='pca', group.by='condition')
-dev.off()
-
-# Determine percent of variation associated with each PC
-pct <- pbmc[["pca"]]@stdev / sum(pbmc[["pca"]]@stdev) * 100
-# Calculate cumulative percents for each PC
-cumu <- cumsum(pct)
-# Determine which PC exhibits cumulative percent greater than 90% and % variation associated with the PC as less than 5
-co1 <- which(cumu > 90 & pct < 5)[1]
-# Determine the difference between variation of PC and subsequent PC
-co2 <- sort(which((pct[1:length(pct) - 1] - pct[2:length(pct)]) > 0.1), decreasing = T)[1] + 1
-# Minimum of the two calculation
-pcs <- min(co1, co2)
-print(paste('Selected # PCs', pcs))
 
 saveRDS(pbmc, 'pbmc.RDS')
-
-# # Being cautious and using 25 PCs
-# pbmc <- FindNeighbors(pbmc,dims=1:25)
-# # Number of clusters in original paper = 19
-# library("leiden")
-# leiden_clustering <- leiden(pbmc@graphs$RNA_snn, resolution_parameter = 0.3)
-# pbmc@meta.data$leiden_clustering <- leiden_clustering
-# Idents(pbmc) <- 'leiden_clustering'
-# pbmc <- RunUMAP(pbmc, dims = 1:25)
-
-# pdf('seurat.clusters.DimPlot.pdf')
-# DimPlot(pbmc, reduction='umap', label=TRUE)
-# dev.off()
 
 # # Remove ambient RNA with decontX
 # pbmc.raw <- CreateSeuratObject(counts=pbmc.data)
