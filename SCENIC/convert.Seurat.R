472d463064c0493b1f76419403099dacbf11eac6
LGGray <l.gray@garvan.org.au>
2024-05-10 16:03:27 +1000

    Fix SCENIC conversion and add h5Seurat conversion in convert.Seurat.R

--- SCENIC/convert.Seurat.R
+++ SCENIC/convert.Seurat.R
@@ -13,3 +13,6 @@ fwrite(data.frame(t(expr_matrix)), 'SCENIC/expr_matrix.csv', row.names = TRUE)
 # pbmc.loom <- as.loom(pbmc, filename = "SCENIC/pbmc.female.loom", verbose = TRUE, overwrite = TRUE)
 # pbmc.loom
 # pbmc.loom$close_all()
+
+SaveH5Seurat(pbmc, filename = "pbmc.female.control-managed.h5Seurat")
+Convert("pbmc.female.control-managed.h5Seurat", dest = "h5ad")
