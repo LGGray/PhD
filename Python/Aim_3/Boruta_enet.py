6e52dd2ebc1166ec7a6f3348712b873d38c0833b
LGGray <l.gray@garvan.org.au>
2024-06-28 17:17:22 +1000

    splitting data

--- Python/Aim_3/Boruta_enet.py
+++ Python/Aim_3/Boruta_enet.py
@@ -73,69 +73,69 @@ y_train.to_csv(f'new_pseudobulk/split_{sys.argv[2]}/data.splits/y_train.'+cell+'
 X_test.to_csv(f'new_pseudobulk/split_{sys.argv[2]}/data.splits/X_test.'+cell+'.csv', index=True)
 y_test.to_csv(f'new_pseudobulk/split_{sys.argv[2]}/data.splits/y_test.'+cell+'.csv', index=True)
 
-### Boruta feature selection ###
-X = X_train.values
-y = y_train.ravel()
-# random forest classifier utilising all cores and sampling in proportion to y labels
-param_grid = {'n_estimators': [100, 200, 300, 400],
-              'criterion': ['gini', 'entropy'],
-              'max_features': ['sqrt', 'log2', 0.3],
-                'max_depth': [3, 4, 5, 6, 7],
-                'min_samples_split': [2, 5, 8, 10]
-}
-clf = RandomForestClassifier(n_jobs=-1, class_weight='balanced', random_state=42)
-grid_search = GridSearchCV(clf, param_grid, 
-                           cv=RepeatedKFold(n_splits=10, n_repeats=3, 
-                                            random_state=42), 
-                                            n_jobs=8, 
-                                            verbose=1,
-                                            scoring='accuracy')
-# Fit the grid search object to the training data
-grid_search.fit(X, y)
-# Create an RFECV object with a random forest classifier
-rf = RandomForestClassifier(n_estimators=grid_search.best_params_['n_estimators'], 
-                            max_features=grid_search.best_params_['max_features'],
-                            max_depth=grid_search.best_params_['max_depth'], 
-                            min_samples_split=grid_search.best_params_['min_samples_split'],
-                            class_weight='balanced',
-                            n_jobs=-1)
-# define Boruta feature selection method
-feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=1)
-# find all relevant features
-feat_selector.fit(X, y)
+# ### Boruta feature selection ###
+# X = X_train.values
+# y = y_train.ravel()
+# # random forest classifier utilising all cores and sampling in proportion to y labels
+# param_grid = {'n_estimators': [100, 200, 300, 400],
+#               'criterion': ['gini', 'entropy'],
+#               'max_features': ['sqrt', 'log2', 0.3],
+#                 'max_depth': [3, 4, 5, 6, 7],
+#                 'min_samples_split': [2, 5, 8, 10]
+# }
+# clf = RandomForestClassifier(n_jobs=-1, class_weight='balanced', random_state=42)
+# grid_search = GridSearchCV(clf, param_grid, 
+#                            cv=RepeatedKFold(n_splits=10, n_repeats=3, 
+#                                             random_state=42), 
+#                                             n_jobs=8, 
+#                                             verbose=1,
+#                                             scoring='accuracy')
+# # Fit the grid search object to the training data
+# grid_search.fit(X, y)
+# # Create an RFECV object with a random forest classifier
+# rf = RandomForestClassifier(n_estimators=grid_search.best_params_['n_estimators'], 
+#                             max_features=grid_search.best_params_['max_features'],
+#                             max_depth=grid_search.best_params_['max_depth'], 
+#                             min_samples_split=grid_search.best_params_['min_samples_split'],
+#                             class_weight='balanced',
+#                             n_jobs=-1)
+# # define Boruta feature selection method
+# feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=1)
+# # find all relevant features
+# feat_selector.fit(X, y)
 
-# Save the model
-filename = f'new_pseudobulk/split_{sys.argv[2]}/feature.select.model/boruta_'+os.path.basename(file).replace('.RDS', '')+'.sav'
-pickle.dump(feat_selector, open(filename, 'wb'))
+# # Save the model
+# filename = f'new_pseudobulk/split_{sys.argv[2]}/feature.select.model/boruta_'+os.path.basename(file).replace('.RDS', '')+'.sav'
+# pickle.dump(feat_selector, open(filename, 'wb'))
 
-# Get feature rankings
-feature_ranks = list(feat_selector.ranking_)
-# Create a DataFrame with features, their importance, and ranks
-feature_df = pd.DataFrame({
-    'Feature': X_train.columns,
-    'Rank': feature_ranks
-})
-# Sort the DataFrame based on feature ranks
-feature_df.sort_values(by='Rank', ascending=True, inplace=True)
-# Save the feature importance to file
-feature_df.to_csv(f'new_pseudobulk/split_{sys.argv[2]}/features/boruta_features.'+cell+'.csv', index=False)
+# # Get feature rankings
+# feature_ranks = list(feat_selector.ranking_)
+# # Create a DataFrame with features, their importance, and ranks
+# feature_df = pd.DataFrame({
+#     'Feature': X_train.columns,
+#     'Rank': feature_ranks
+# })
+# # Sort the DataFrame based on feature ranks
+# feature_df.sort_values(by='Rank', ascending=True, inplace=True)
+# # Save the feature importance to file
+# feature_df.to_csv(f'new_pseudobulk/split_{sys.argv[2]}/features/boruta_features.'+cell+'.csv', index=False)
 
-### Elastic net feature selection ###
-ratios = arange(0, 1.1, 0.1)
-alphas = np.logspace(-4, 0, 10)
-cv=RepeatedKFold(n_splits=10, n_repeats=3, random_state=42)
-enet = ElasticNetCV(l1_ratio=ratios, alphas=alphas, cv=cv, n_jobs=8, random_state=42)
-enet.fit(X_train, y_train.ravel())
-print(enet)
+# ### Elastic net feature selection ###
+# ratios = arange(0, 1.1, 0.1)
+# alphas = np.logspace(-4, 0, 10)
+# cv=RepeatedKFold(n_splits=10, n_repeats=3, random_state=42)
+# enet = ElasticNetCV(l1_ratio=ratios, alphas=alphas, cv=cv, n_jobs=8, random_state=42)
+# enet.fit(X_train, y_train.ravel())
+# print(enet)
 
-# Create a dataframe of the features and their coefficients
-enet_features = pd.DataFrame({'Feature': enet.feature_names_in_, 'coef': enet.coef_})
-# Save the features to file
-enet_features.to_csv(f'new_pseudobulk/split_{sys.argv[2]}/features/enet_features.'+cell+'.csv', index=False)
+# # Create a dataframe of the features and their coefficients
+# enet_features = pd.DataFrame({'Feature': enet.feature_names_in_, 'coef': enet.coef_})
+# # Save the features to file
+# enet_features.to_csv(f'new_pseudobulk/split_{sys.argv[2]}/features/enet_features.'+cell+'.csv', index=False)
 
-# Save the model
-filename = f'new_pseudobulk/split_{sys.argv[2]}/feature.select.model/enet_'+os.path.basename(file).replace('.RDS', '')+'.sav'
-pickle.dump(enet, open(filename, 'wb'))
+# # Save the model
+# filename = f'new_pseudobulk/split_{sys.argv[2]}/feature.select.model/enet_'+os.path.basename(file).replace('.RDS', '')+'.sav'
+# pickle.dump(enet, open(filename, 'wb'))
 
 end_time = time.time()
 cpu_time = end_time - start_time
