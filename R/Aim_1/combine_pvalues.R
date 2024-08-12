3ee5257eeee6c285117445be713aa90544b95b25
LGGray <l.gray@garvan.org.au>
2024-08-12 10:53:33 +1000

    Refactor R code to combine p-values from DEG across studies

--- R/Aim_1/combine_pvalues.R
+++ R/Aim_1/combine_pvalues.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(purrr)
 library(metap)
+library(metafor)
+library(ComplexHeatmap)
+library(circlize)
 
 source('/directflow/SCCGGroupShare/projects/lacgra/PhD/functions/edgeR.list.R')
 source('/directflow/SCCGGroupShare/projects/lacgra/PhD/functions/replace.names.R')
@@ -9,6 +12,9 @@ chrX <- read.delim('/directflow/SCCGGroupShare/projects/lacgra/datasets/XCI/chrX
 chrX <- subset(chrX, Gene.name != '')
 chrX <- chrX$Gene.name
 
+load('/directflow/SCCGGroupShare/projects/lacgra/datasets/XCI/escapees.Rdata')
+tukiainen <- read.delim('/directflow/SCCGGroupShare/projects/lacgra/datasets/XCI/Tukiainen.escape.txt')
+
 pSS <- deg.list('pSS_GSE157278/differential.expression/edgeR/', filter=FALSE)
 UC <- deg.list('UC_GSE125527/differential.expression/edgeR/', filter=FALSE)
 CD_colon <- deg.list('CD_Kong/colon/differential.expression/edgeR/', filter=FALSE)
@@ -16,7 +22,7 @@ names(CD_colon)[2] <- 'CD16-_NK_cells'
 CD_TI <- deg.list('CD_Kong/TI/differential.expression/edgeR/', filter=FALSE)
 names(CD_TI)[1] <- 'CD16-_NK_cells'
 SLE <- deg.list('lupus_Chun/differential.expression/edgeR/', filter=FALSE)
-# MS <- deg.list('MS_GSE193770/differential.expression/edgeR', filter=FALSE)
+MS <- deg.list('MS_GSE193770/differential.expression/edgeR', filter=FALSE)
 
 common <- Reduce(intersect, list(names(pSS), names(UC), names(CD_colon), names(CD_TI), names(SLE)))
 
@@ -24,29 +30,69 @@ combined_fdr_list <- list()
 for(celltype in common){
 
     df_list <- list('pSS'=pSS[[celltype]], 'UC'=UC[[celltype]], 
-        'CO'=CD_colon[[celltype]], 'TI'=CD_TI[[celltype]], 'SLE'=SLE[[celltype]])
+        'CO'=CD_colon[[celltype]], 'TI'=CD_TI[[celltype]], 'SLE'=SLE[[celltype]],
+        'MS'=MS[[celltype]])
+    
+    df_list <- df_list[sapply(df_list, function(x) is.null(x) == FALSE)]
 
     df <- df_list %>%
         imap(function(x, y) x %>% rename_with(~paste(., y, sep = '_'), -gene)) %>%
         reduce(full_join, by = 'gene') %>%
         data.frame()
 
-    df_fdr <- df[,c('FDR_pSS', 'FDR_UC', 'FDR_CO', 'FDR_TI', 'FDR_SLE')]
+    df_fdr <- df[,grep('FDR', colnames(df))]
     df_fdr[is.na(df_fdr)] <- 1
 
-    df_logFC <- df[,c('logFC_pSS', 'logFC_UC', 'logFC_CO', 'logFC_TI', 'logFC_SLE')]
+    df_logFC <- df[,grep('logFC', colnames(df))]
     df_logFC[is.na(df_logFC)] <- 0
 
     combined_fdr <- apply(df_fdr, 1, function(x) sumlog(x)$p)
-    combined_logFC <- apply(df_logFC, 1, function(x) ifelse(mean(x) > 0, 'up', 'down'))
+    combined_logFC <- apply(df_logFC, 1, mean)
 
     combined_fdr_list[[celltype]] <- data.frame(gene = df$gene, 
         combined_fdr = combined_fdr, combined_logFC = combined_logFC)
 }
 
-lapply(combined_fdr_list, function(x){
-    nrow(x[x$gene %in% chrX & x$combined_fdr < 0.05 & x$combined_logFC == 'up',])
+save(combined_fdr_list, file = 'Aim_1/combined_fdr_list.Rdata')
+
+
+degs_list <- lapply(combined_fdr_list, function(x){
+    subset(x, combined_fdr < 0.01 & abs(combined_logFC) > 0.1)
 })
+degs <- bind_rows(degs_list, .id='celltype')
+
+degs_wide <- dcast(gene ~ celltype, value=combined_logFC, data=degs)
+degs_wide[is.na(degs_wide)] <- 0
+rownames(degs_wide) <- degs_wide$gene
+colnames(degs_wide) <- replace.names(gsub('_', '.', colnames(degs_wide)))
+degs_wide <- degs_wide[,-1]
+
+
+pdf('Aim_1/combined_logFC_sig_heatmap.pdf')
+col <- colorRamp2(c(-1, 0, 1), c('blue', 'white', 'red'))
+Heatmap(as.matrix(degs_wide), col = col, name = 'combined_logFC',
+show_row_names=FALSE) 
+dev.off()
+
+
+
+X.genes <- lapply(combined_fdr_list, function(x){
+    x[x$gene %in% chrX & x$combined_fdr < 0.05,'gene']
+})
+load('../datasets/XCI/escapees.Rdata')
+lapply(X.genes, function(x) length(x[x %in% rownames(escape)])/length(x))
+
+lapply(X.genes, function(x) x[x %in% rownames(escape)])
+
+fishers.test.combined <- function(data, genes, logfc){
+    a <- nrow(data[data$combined_fdr < 0.05 & data$gene %in% genes & abs(data$combined_logFC) > logfc,])
+    b <- nrow(data[data$combined_fdr < 0.05 & !data$gene %in% genes & abs(data$combined_logFC) > logfc,])
+    c <- nrow(data[data$combined_fdr >= 0.05 & data$gene %in% genes,])
+    d <- nrow(data[data$combined_fdr >= 0.05 & !data$gene %in% genes,])
+    return(fisher.test(matrix(c(a, b, c, d), nrow = 2 ))$p.value)
+}
+
+lapply(combined_fdr_list, function(x) fishers.test.combined(x, rownames(escape), 0.1))
 
 combined_fdr <- bind_rows(combined_fdr_list, .id = 'celltype')
 subset_combined_fdr <- subset(combined_fdr, gene %in% chrX & combined_fdr < 0.05)
